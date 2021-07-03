bd1c4c3f60abf077253426388f42fa574c8974a4
auleki <kodagiwa@gmail.com>
2021-07-03 21:23:57 +0100

    setting up and reviewing basic concepts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,107 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header title="Tasks Tracker" />
+  <Tasks @delete-task="deleteTask" :tasks="tasks" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      console.log('task', id)
+    }
+  },
+  created () {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor's appointment",
+        day: 'June 6th at 3pm',
+        reminder: false
+      },
+      {
+        id: 2,
+        text: "Wogorowogoro with babe",
+        day: 'June 29th at 9pm',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: "Call Ntongha",
+        day: 'June 30th at 1pm',
+        reminder: true
+      },
+      {
+        id: 4,
+        text: "Make a new application",
+        day: 'July 6th at 10pm',
+        reminder: true
+      },
+      {
+        id: 5,
+        text: "Give tailor new cloth",
+        day: 'July 14th at 3pm',
+        reminder: false
+      }      
+    ]
   }
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,63 @@
+<template>
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <div class="task__title">
+      <h5>{{ task.text }}</h5>
+      <i @click="deleteTask(task.id)" class="fas fa-times"></i>
+    </div>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    task: Object
+  },
+  methods: {
+    deleteTask (id) {
+      this.$emit('delete-task', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .fas {
+    color: red;
+    cursor: pointer;
+    font-size: 1.2em;
+    transition: 200ms ease-in;
+  }
+
+  .fas:hover {
+    color: #fff;
+    transform: rotateZ(180deg);
+  }
+
+  p {
+    color: rgb(197, 197, 197);
+  }
+
+  .task {
+    background: rgb(17, 17, 17);
+    color: rgb(255, 255, 255);
+    padding: 1em;
+    border-left: solid transparent 3px;
+    margin: 1em 0;
+    border-radius: .3em;
+  }
+
+  .reminder {
+    border-color: rgb(255, 136, 0);
+  }
+
+  .task__title {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  h3 {
+    display: inline;
+  }
+</style>
